5017cec8e0a1cd205af32196b1613e76d1da9300
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-12 16:00:07 +0700

    Fix permission

--- backend/main/permissions/todo.py
+++ backend/main/permissions/todo.py
@@ -2,8 +2,9 @@ from rest_framework import permissions
 
 
 class IsOwnerPermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return obj.user == request.user
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
         return obj.user == request.user
--- backend/to_do/views.py
+++ backend/to_do/views.py
@@ -1,4 +1,3 @@
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from main.permissions.todo import IsOwnerPermission
@@ -8,16 +7,8 @@ from to_do.serializers import TodoSerializer
 
 class TodoViewSet(ModelViewSet):
     serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
     permission_classes = [IsOwnerPermission, ]
 
-    def list(self, request, *args, **kwargs):
-        queryset = Todo.objects.filter(user=request.user)
-        page = self.paginate_queryset(queryset)
-
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
